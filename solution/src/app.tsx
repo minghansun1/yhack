3e1946525ba5989466dd012cdc6dbe2e2b7b3fe3
minghansun1 <minghansun@hotmail.com>
2024-10-05 16:38:55 -0400

    movable marker

---
+++ solution/src/app.tsx
@@ -0,0 +1,161 @@
+import React, {useEffect, useState, useRef, useCallback} from 'react';
+import {createRoot} from 'react-dom/client';
+
+import {
+  APIProvider,
+  Map,
+  useMap,
+  AdvancedMarker,
+  MapCameraChangedEvent,
+  Pin
+} from '@vis.gl/react-google-maps';
+
+import {MarkerClusterer} from '@googlemaps/markerclusterer';
+import type {Marker} from '@googlemaps/markerclusterer';
+
+import {Circle} from './components/circle'
+
+type Poi ={ key: string, location: google.maps.LatLngLiteral }
+const locations: Poi[] = [
+  {key: 'operaHouse', location: { lat: -33.8567844, lng: 151.213108  }},
+  {key: 'tarongaZoo', location: { lat: -33.8472767, lng: 151.2188164 }},
+  {key: 'manlyBeach', location: { lat: -33.8209738, lng: 151.2563253 }},
+  {key: 'hyderPark',  location: { lat: -33.8690081, lng: 151.2052393 }},
+  {key: 'theRocks',   location: { lat: -33.8587568, lng: 151.2058246 }},
+  {key: 'circularQuay', location: { lat: -33.858761, lng: 151.2055688 }},
+  {key: 'harbourBridge', location: { lat: -33.852228, lng: 151.2038374 }},
+  {key: 'kingsCross', location: { lat: -33.8737375, lng: 151.222569 }},
+  {key: 'botanicGardens', location: { lat: -33.864167, lng: 151.216387 }},
+  {key: 'museumOfSydney', location: { lat: -33.8636005, lng: 151.2092542 }},
+  {key: 'maritimeMuseum', location: { lat: -33.869395, lng: 151.198648 }},
+  {key: 'kingStreetWharf', location: { lat: -33.8665445, lng: 151.1989808 }},
+  {key: 'aquarium', location: { lat: -33.869627, lng: 151.202146 }},
+  {key: 'darlingHarbour', location: { lat: -33.87488, lng: 151.1987113 }},
+  {key: 'barangaroo', location: { lat: - 33.8605523, lng: 151.1972205 }},
+];
+
+const App = () => {
+  const mapRef = useRef(null); // Reference for the map container
+
+  useEffect(() => {
+    // Function to initialize the map
+    const initMap = async () => {
+      // Request needed libraries from Google Maps
+      const { Map, InfoWindow } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+      const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
+
+      // Create a new map instance and attach it to the mapRef div
+      const map = new Map(mapRef.current, {
+        center: { lat: 37.39094933041195, lng: -122.02503913145092 },
+        zoom: 5,
+        mapId: '4504f8b37365c3d0',
+      });
+
+      const infoWindow = new InfoWindow();
+
+      // Create a draggable marker
+      const draggableMarker = new AdvancedMarkerElement({
+        map,
+        position: { lat: 37.39094933041195, lng: -122.02503913145092 },
+        gmpDraggable: true,
+        title: "This marker is draggable.",
+      });
+
+      // Add event listener for when the marker is dropped
+      draggableMarker.addListener('dragend', (event) => {
+        const position = draggableMarker.position as google.maps.LatLng;
+        infoWindow.close();
+        infoWindow.setContent(`Pin dropped at: ${position.lat()}, ${position.lng()}`);
+
+        
+        infoWindow.open(draggableMarker.map, draggableMarker);
+      });
+    };
+
+    // Call the initMap function after the component mounts
+    initMap();
+  }, []); // Empty dependency array means this effect runs once after the component mounts
+
+  return (
+    // Map container, will be assigned to mapRef
+    <div ref={mapRef} style={{ height: '500px', width: '100%' }}>
+      {/* The map will be rendered inside this div */}
+    </div>
+  );
+};
+
+
+const PoiMarkers = (props: { pois: Poi[] }) => {
+  const map = useMap();
+  const [markers, setMarkers] = useState<{[key: string]: Marker}>({});
+  const clusterer = useRef<MarkerClusterer | null>(null);
+  const [circleCenter, setCircleCenter] = useState(null)
+  const handleClick = useCallback((ev: google.maps.MapMouseEvent) => {
+    if(!map) return;
+    if(!ev.latLng) return;
+    console.log('marker clicked: ', ev.latLng.toString());
+    map.panTo(ev.latLng);
+    setCircleCenter(ev.latLng);
+  });
+  // Initialize MarkerClusterer, if the map has changed
+  useEffect(() => {
+    if (!map) return;
+    if (!clusterer.current) {
+      clusterer.current = new MarkerClusterer({map});
+    }
+  }, [map]);
+
+  // Update markers, if the markers array has changed
+  useEffect(() => {
+    clusterer.current?.clearMarkers();
+    clusterer.current?.addMarkers(Object.values(markers));
+  }, [markers]);
+
+  const setMarkerRef = (marker: Marker | null, key: string) => {
+    if (marker && markers[key]) return;
+    if (!marker && !markers[key]) return;
+
+    setMarkers(prev => {
+      if (marker) {
+        return {...prev, [key]: marker};
+      } else {
+        const newMarkers = {...prev};
+        delete newMarkers[key];
+        return newMarkers;
+      }
+    });
+  };
+
+  return (
+    <>
+      <Circle
+          radius={800}
+          center={circleCenter}
+          strokeColor={'#0c4cb3'}
+          strokeOpacity={1}
+          strokeWeight={3}
+          fillColor={'#3b82f6'}
+          fillOpacity={0.3}
+        />
+      {props.pois.map( (poi: Poi) => (
+        <AdvancedMarker
+          key={poi.key}
+          position={poi.location}
+          ref={marker => setMarkerRef(marker, poi.key)}
+          clickable={true}
+          onClick={handleClick}
+          >
+            <Pin background={'#FBBC04'} glyphColor={'#000'} borderColor={'#000'} />
+        </AdvancedMarker>
+      ))}
+    </>
+  );
+};
+
+export default App;
+
+const root = createRoot(document.getElementById('app'));
+root.render(
+      <App />
+  );
+
