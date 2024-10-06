643e25bba33097e4ed4c4c1b3dc0e68881d9cf8f
untrivial <axc@wharton.upenn.edu>
2024-10-06 03:03:30 -0400

    final

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -22,7 +22,7 @@ import { Circle } from './components/circle';
 
 
 // Map Component (previously your main App component)
-const MapPage = () => {
+const MapPage = ({ setLatitude, setLongitude, setCloudCover, setLight, setLabel, setHeight }) => {
   const mapRef = useRef(null); // Reference for the map container
 
   useEffect(() => {
@@ -37,6 +37,25 @@ const MapPage = () => {
         mapId: '4504f8b37365c3d0',
       });
 
+      const getElevation = async (latitude, longitude) => {
+        const elevator = new window.google.maps.ElevationService();
+        const locations = [{ lat: latitude, lng: longitude }];
+
+        return new Promise((resolve, reject) => {
+          elevator.getElevationForLocations({
+            locations: locations
+          }, (results, status) => {
+            if (status === window.google.maps.ElevationStatus.OK && results && results[0]) {
+              const elevation = results[0].elevation;
+              resolve(elevation);
+            } else {
+              console.error('Elevation service failed due to:', status);
+              reject('Elevation service failed');
+            }
+          });
+        });
+      };
+
       const infoWindow = new InfoWindow();
 
       const draggableMarker = new AdvancedMarkerElement({
@@ -50,6 +69,8 @@ const MapPage = () => {
         const position = draggableMarker.position as google.maps.LatLng;
         const lat = position.lat;
         const lng = position.lng;
+        setLatitude(lat);
+        setLongitude(lng);
 
         infoWindow.close();
         const contentString = `
@@ -74,27 +95,39 @@ const MapPage = () => {
                   longitude: lng
                 }
               })
-                .then(response => {
+                .then(async (response) => {
                   const responseData = response.data;
                   console.log(responseData);
                   const lightScore = Math.max(0, Math.min(100, (30 - responseData.light) * 10 / 3));
+                  setLight(responseData.light);
                   const visibilityScore = Math.max(0, Math.min(100, responseData.visibility / 100));
                   const cloudCoverScore = Math.max(0, Math.min(100, 100 - responseData.clouds.all));
-                  const compositeScore = ((lightScore*visibilityScore*cloudCoverScore)/(10**6)).toFixed(2);
-                  console.log(lightScore, visibilityScore, cloudCoverScore);
+                  setCloudCover(responseData.clouds.all);
+                  
+                  var elevation = await getElevation(lat, lng);
+                  const elevationScore = Math.max(0, Math.min(100, elevation/25));
+                  setHeight(elevation);
+                  const compositeScore = ((0.9*(lightScore * visibilityScore * cloudCoverScore) / (10**6)) + elevationScore/1000).toFixed(2);
+                  console.log(lightScore, visibilityScore, cloudCoverScore, elevation);
 
                   function getCompositeScoreLabel(score) {
                     if (score <= 0.2) {
+                      setLabel("Horrible");
                       return { label: "Horrible", color: "red" };
                     } else if (score <= 0.4) {
+                      setLabel("Bad");
                       return { label: "Bad", color: "orange" };
                     } else if (score <= 0.6) {
+                      setLabel("Mid");
                       return { label: "Mid", color: "yellow" };
                     } else if (score <= 0.8) {
+                      setLabel("Good");
                       return { label: "Good", color: "lightgreen" };
                     } else if (score <= 0.95) {
+                      setLabel("Great");
                       return { label: "Great", color: "green" };
                     } else {
+                      setLabel("Pristine");
                       return { label: "Pristine", color: "darkgreen" };
                     }
                   }
@@ -143,6 +176,16 @@ const MapPage = () => {
                     </div>
                     <p>${responseData.clouds.all}%</p>
 
+                    <!-- Elevation -->
+                    <p>Elevation:</p>
+                    <div style="background: linear-gradient(to right, red, green); height: 10px; width: 100%; position: relative;">
+                      <!-- Arrow indicator for elevation -->
+                      <div style="position: absolute; left: ${Math.min(98, elevationScore)}%; top: -15px;">
+                        ▲
+                      </div>
+                    </div>
+                    <p>${elevation} meters</p>
+
                     <!-- Button -->
                     <button id="infoWindowButton">See Stargazing Quality</button>
                   </div>
@@ -174,11 +217,19 @@ const MapPage = () => {
 const App = () => {
   const [textBoxContent, setTextBoxContent] = useState('');
   const [chatInput, setChatInput] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  const [cloudCover, setCloudCover] = useState('');
+  const [light, setLight] = useState('');
+  const [label, setLabel] = useState('');
+  const [height, setHeight] = useState('');
+  console.log("label: " + label);
+  const context = "You are a helpful and concise stargazing assistant. All of your responses should be related to stargazing. You are given the following information about the user's location: " + latitude + ", " + longitude + ". You are also given the following information about the cloud cover at the user's location: " + cloudCover + "% cloud cover. Include any outside knowledge about the user's location based on the coordinates. For example, you might suggest nearby locations for stargazing. The light level at the user's location is " + light + ". A high light level is 30 and a low/no light level is 0. The elevation at the user's location is " + height + " meters. The computed label of the stargazing quality at the user's location is: " + label + ". Rely on this label. Also keep in mind that light is the most important factor, followed by cloud cover, and then elevation. Keep your response short and succinct."
 
   const handleChatSubmit = (e: React.FormEvent) => {
     axios.get('http://127.0.0.1:5000/chat', {
       params: {
-        input: chatInput
+        input: context + chatInput
       }
     })
     .then(response => {
@@ -201,7 +252,7 @@ const App = () => {
         <div style={{ display: 'flex', flex: 1 }}>
           <div style={{ flex: 1 }}>
             <Routes>
-              <Route path="/" element={<MapPage />} />
+              <Route path="/" element={<MapPage setLatitude={setLatitude} setLongitude={setLongitude} setCloudCover={setCloudCover} setLight={setLight} setLabel={setLabel} setHeight={setHeight} />} />
               <Route path="/about" element={<About />} />
               <Route path="/resources" element={<Resources />} />
             </Routes>
@@ -213,8 +264,8 @@ const App = () => {
                 type="text"
                 value={chatInput}
                 onChange={(e) => setChatInput(e.target.value)}
-                placeholder="Ask a question..."
-                style={{ width: '100%', padding: '10px', marginBottom: '10px' }}
+                placeholder="How is my location for stargazing?"
+                style={{ width: '90%', padding: '10px', marginBottom: '10px' }}
               />
               <button type="submit" style={{ width: '100%', padding: '10px' }}>Send</button>
             </form>
